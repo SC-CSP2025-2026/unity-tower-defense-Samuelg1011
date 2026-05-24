baeb1c2f0d3eee5d72c642b502f9eb8d203a0c4a
Samuelg1011 <guosamuel178@gmail.com>
2026-05-24 11:16:50 -0700

    feat: completed enemy spawner challenge

--- 塔防/Assets/Scripts/EnemySpawn.cs
+++ 塔防/Assets/Scripts/EnemySpawn.cs
@@ -11,10 +11,23 @@ public class EnemySpawn : MonoBehaviour
     [field: SerializeField]
     public float Delay {get; private set; } = 5f;
 
+    [field: SerializeField]
+    public float SpawnsRemaining { get; private set; } = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+       
+    }
+
+    void OnEnable()
+    {
         InvokeRepeating(nameof(Spawn), Delay, Delay);
+    }
+    
+
+    void OnDisable()
+    {
+        CancelInvoke();
     }
 
     // Update is called once per frame
@@ -27,5 +40,12 @@ public class EnemySpawn : MonoBehaviour
     {
         EnemyMovement newEnemy = Object.Instantiate(Enemy);
         newEnemy.Target = StartingWaypoint;
+        SpawnsRemaining--;//SpawnsRemaining -= 1;// SpawnsRemaining = SpawnsRemaining - 1;
+        if (SpawnsRemaining == 0)
+        {
+            CancelInvoke();
+        }
     }
+
+
 }
