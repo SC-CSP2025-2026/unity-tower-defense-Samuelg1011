c38f965e89f6002e7f51ef6606a144c0a81021aa
Samuelg1011 <guosamuel178@gmail.com>
2026-05-27 00:09:59 -0700

    feat: finished 12.5

--- 塔防/Assets/Scripts/Health.cs
+++ 塔防/Assets/Scripts/Health.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
@@ -8,11 +9,15 @@ public class Health : MonoBehaviour
     [field: SerializeField]
     public float Damage { get; private set; }
 
+    [field: SerializeField]
+    public UnityEvent<Health> OnDeath { get; private set; }
+
     public void ApplyHit(Projectile projectile)
     {
         Damage += projectile.Damage;
         if (Damage >= BaseHealth)
         {
+            OnDeath.Invoke(this);
             Object.Destroy(gameObject);
         }
     }
